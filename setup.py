ae3e2a0ff9081e83fb1017ada6f3c203e9b3570c
michalkalkowski <kalkowski.m@gmail.com>
2018-01-24 16:28:59 +0000

    Complete restructurisation. Learning, adjusting. Now supports conda environment and autoenv

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="mina-weld",
+    description="An implementation of the MINA model for multipass austenitic steel welds",
+    author="Michal K Kalkowski",
+    author_email="m.kalkowski@imperial.ac.uk",
+    packages=find_packages(exclude=['data', 'references', 'output', 'notebooks']),
+    long_description=read('README.md'),
+    license='MIT'
+)
